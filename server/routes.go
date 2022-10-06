e3b3e013b25671f27b70a4e530101773cbdc97eb
Yuri Cavalcante <58712896+yuriwc@users.noreply.github.com>
2022-10-05 22:49:08 -0300

    Update routes.go

--- server/routes.go
+++ server/routes.go
@@ -17,20 +17,24 @@ func ConfigRoutes(router *gin.Engine) *gin.Engine {
 			//r.POST("/createUser", controllers.CriarUsuario)
 			user.POST("/getUser", controllers.GetUsuario)
 			//r.GET("/getPessoa/:numero", controllers.GetPessoaByNumberController)
+			user.POST("/updateIdPessoa", controllers.UpdateIdPessoaOnUser)
 		}
 		pessoa := main.Group("pessoa", middlewares.Auth())
 		{
 			pessoa.POST("/createPessoa", controllers.CreatePessoa)
 			pessoa.POST("/createAddress", controllers.CriarEndereco)
+			pessoa.POST("/getGanhador", controllers.GetGanhadorFromDB)
 		}
 		bicho := main.Group("bicho", middlewares.Auth())
 		{
 			bicho.POST("/createBicho", controllers.CriarBicho)
+			bicho.GET("/getAllBichos", controllers.GetAllBichos)
 		}
 		bilhete := main.Group("bilhete", middlewares.Auth())
 		{
 			bilhete.POST("/create", controllers.CriarBilhete)
 			bilhete.PUT("/update", controllers.UpdateBilhete)
+			bilhete.GET("/getByPersonId", controllers.GetAllBilhetesFromAPerson)
 		}
 		numeroBilhete := main.Group("numeroBilhete", middlewares.Auth())
 		{
